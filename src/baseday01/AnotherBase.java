9c090abf0a80343a0e269eae866d83e08ed1ff94
youarelying <kronos_tx@163.com>
2020-12-23 14:53:48 +0800

    1223 Git Share Test

---
+++ src/baseday01/AnotherBase.java
@@ -0,0 +1,15 @@
+package baseday01;
+
+public class AnotherBase {
+    public static void main(String[] args) {
+        System.out.println("20190101");
+        System.out.println("");
+
+
+
+
+
+
+
+    }
+}
